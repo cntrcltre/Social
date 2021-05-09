0126182ae873f4c0dbb8242ec2c708f21b5fa6c6
cntrcltre <anton.bindasov@gmail.com>
2021-05-09 14:34:59 +0300

    Добавьте файлы проекта.

---
+++ Social/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Social.Areas.Identity.Data;
+using Social.Data;
+
+[assembly: HostingStartup(typeof(Social.Areas.Identity.IdentityHostingStartup))]
+namespace Social.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequiredLength = 3;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireLowercase = false;
+                    options.User.RequireUniqueEmail = true;
+                })
+                    .AddRoles<IdentityRole>()
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
